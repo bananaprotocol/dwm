8644dbfddd82ffe59e800a60876c2364fcf5925b
bananaprotocol <bananaprotocol@protonmail.com>
2019-10-13 22:03:45 +0200

    Seperate themes into header files

--- config.def.h
+++ config.def.h
@@ -8,19 +8,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Term:size=10" };
 static const char dmenufont[]       = "Iosevka Term:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+/*
+ * themes
+ */
+#include "themes/default.h"
+/* #include "/home/<USER>/.cache/wal/colors-wal-dwm.h" // use pywal colors (replace <USER> with your username) */
+
 static const unsigned int baralpha = 0xe6;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeUrg]  = { col_gray4, col_cyan,  "#ff0000" },
-};
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
